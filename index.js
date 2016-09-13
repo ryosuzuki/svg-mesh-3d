a11be059271ce9ef1116db3fbf0335ff45768b41
Ryo Suzuki <ryosuzk@gmail.com>
2016-09-13 11:48:06 -0600

    Add triangulate contour

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ var normalize = require('normalize-path-scale')
 var random = require('random-float')
 var assign = require('object-assign')
 var simplify = require('simplify-path')
+var triangulateContours = require('triangulate-contours')
 
 module.exports = svgMesh3d
 function svgMesh3d (svgPath, opt) {
   if (typeof svgPath !== 'string') {
     throw new TypeError('must provide a string as first parameter')
   }
-  
+
   opt = assign({
     delaunay: true,
     clean: true,
     exterior: false,
     randomization: 0,
     simplify: 0,
-    scale: 1
+    scale: 1,
+    customize: false,
   }, opt)
-  
+
   var i
   // parse string as a list of operations
   var svg = parseSVG(svgPath)
-  
   // convert curves into discrete points
   var contours = getContours(svg, opt.scale)
-  
   // optionally simplify the path for faster triangulation and/or aesthetics
   if (opt.simplify > 0 && typeof opt.simplify === 'number') {
     for (i = 0; i < contours.length; i++) {
       contours[i] = simplify(contours[i], opt.simplify)
     }
   }
-  
+
   // prepare for triangulation
   var polyline = denestPolyline(contours)
   var positions = polyline.positions
@@ -47,7 +47,7 @@ function svgMesh3d (svgPath, opt) {
   if (typeof randomization === 'number' && randomization > 0) {
     addRandomPoints(positions, bounds, randomization)
   }
-  
+
   var loops = polyline.edges
   var edges = []
   for (i = 0; i < loops.length; ++i) {
@@ -57,25 +57,28 @@ function svgMesh3d (svgPath, opt) {
     }
   }
 
-  // this updates points/edges so that they now form a valid PSLG 
+  // this updates points/edges so that they now form a valid PSLG
   if (opt.clean !== false) {
     cleanPSLG(positions, edges)
   }
 
   // triangulate mesh
   var cells = cdt2d(positions, edges, opt)
-
-  // rescale to [-1 ... 1]
-  if (opt.normalize !== false) {
+  if (opt.customize == false) {
+    // rescale to [-1 ... 1]
     normalize(positions, bounds)
+    // convert to 3D representation and flip on Y axis for convenience w/ OpenGL
+    // to3D(positions)
+  } else {
+    var shape = triangulateContours(contours);
+    positions = shape.positions;
+    cells = shape.cells;
   }
-
-  // convert to 3D representation and flip on Y axis for convenience w/ OpenGL
-  to3D(positions)
 
   return {
     positions: positions,
-    cells: cells
+    cells: cells,
+    edges: edges
   }
 }
 
